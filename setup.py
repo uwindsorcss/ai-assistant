af86579919d73b8b0f7aaf90442734034c8584a5
Calder Johnson <calderbjohnson@icloud.com>
2024-06-27 15:21:58 -0400

    feat: python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Enables installation as a Python package."""
+from setuptools import setup, find_packages
+
+setup(
+    name='uwin_ai_automod',
+    version='0.1',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "pandas",
+        "openai",
+        "qdrant-client",
+        "fastapi",
+    ]
+)
