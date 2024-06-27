487a9921f874a16e4aa4ae56c41dadaa7c4e714b
Calder Johnson <calderbjohnson@icloud.com>
2024-06-27 15:34:43 -0400

    Fixed package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='uwin_ai_automod',
+    name='uwin_ai_assistant',
     version='0.1',
     package_dir={"": "src"},
     packages=find_packages(where="src"),
