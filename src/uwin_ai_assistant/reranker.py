4eecd80d04cff5ed03f1d615ed91597371bf50e2
Calder Johnson <calderbjohnson@icloud.com>
2024-07-06 12:55:14 -0400

    fix: removed debug print statements

--- src/uwin_ai_assistant/reranker.py
+++ src/uwin_ai_assistant/reranker.py
@@ -3,9 +3,6 @@ from .clients import cohere_client
 
 def rerank(query, docs):
     """Rerank the documents"""
-    print("OG: \n", "\n\n".join([doc.payload["page_content"] for doc in docs]))
-    print("\n\n")
-    
     reranked_docs = cohere_client.rerank(
         query=query,
         documents=[doc.payload["page_content"] for doc in docs],
@@ -14,10 +11,6 @@ def rerank(query, docs):
         return_documents=True,
     )
     
-    print(f"test: {reranked_docs.results}\n")
     reranked_docs = [result.document.text for result in reranked_docs.results]
-
-    print("Reranked: ", "\n\n".join(reranked_docs))
-    print("\n\n")
 
     return reranked_docs
