475d964f42c7e44071613359057a5eccd81b4499
Calder <calderbjohnson@icloud.com>
2024-07-25 14:57:45 -0400

    Feat: fixed requirements, updated model to GPT-4o-mini

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='uwin_ai_assistant',
-    version='0.1',
+    version='0.2',
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
--- src/uwin_ai_assistant/config.py
+++ src/uwin_ai_assistant/config.py
@@ -9,7 +9,7 @@ COHERE_API_KEY = os.getenv("COHERE_API_KEY")  # API key for Cohere models
 # Locators
 DATA_PATH = "./data/corpus/corpus.csv"  # Path to the input corpus
 VECTOR_DB_URL = "https://797a91bd-7ef2-4373-9dde-54ef042bbc74.us-east4-0.gcp.cloud.qdrant.io:6333"  # URL for the vector database
-GPT_MODEL = "gpt-3.5-turbo-0125"  # URL for the GPT model (will be updated to our fine-tuned model in the future)
+GPT_MODEL = "gpt-4o-mini"  # URL for the GPT model (will be updated to our fine-tuned model in the future)
 EMBEDDING_MODEL = "text-embedding-3-small"  # URL for the OpenAI embeddings model (will be updated to our fine-tuned model in the future)
 RERANK_MODEL = "rerank-english-v3.0"  # URL for the reranking model (will be updated to our fine-tuned model in the future)
 
